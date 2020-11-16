eba66110728e2575b9eff651dfe26b84fbb9755f
daujerrine <aghoruI@teknik.io>
2020-11-16 23:18:14 +0530

    ref/dict.c: update

---
+++ ref/dict.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020 Anamitra Ghorui
+ * This file is part of Calcium. See Calcium's licensing information for
+ * details.
+ */
+
+/*
+ * Dictionary implementation
+ *
+ */
+
+typedef struct Dictionary {
+    
+} Dictionary;
+
+
+int hash(char *key)
+{
+    
+    return 0;
+}
+
+int insert(char *key, int val)
+{
+    
+}
