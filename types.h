ad571be2cc65b6d1a78eaffc29d840b120d03ef7
daujerrine <aghoruI@teknik.io>
2020-12-02 10:53:29 +0530

    types.h: Redo type and variable implementation

--- types.h
+++ types.h
@@ -29,6 +29,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+/// Default Charater Datatype (Non UTF-8)
+typedef uint8_t CaChar;
+
 /// Default integer datatype.
 typedef int64_t CaInt;
 
@@ -41,27 +44,42 @@ typedef long double CaReal;
 /// Default datatupe for any sort of "size" quantity.
 typedef size_t CaSize;
 
+/// Default datatype for mutable objects
+typedef void * CaObjPtr;
+
 /// Datatype for hashtable keys.
 typedef const uint8_t * CaHashKey;
 
 /// Defines types that are used by the interpreter.
+/// All types except for OBJ (classes) are directly handled by the system.
 typedef enum CaType {
     CA_TYPE_UNKNOWN = 0,
     CA_TYPE_REAL,
     CA_TYPE_INT,
-    CA_TYPE_WORD,
-    CA_TYPE_KEY,
-    CA_TYPE_OPER
+    CA_TYPE_STRING,
+    CA_TYPE_DICT,
+    CA_TYPE_SET,
+    CA_TYPE_LIST,
+    CA_TYPE_TUPLE,
+    CA_TYPE_OBJ
 } CaType;
 
-typedef enum CaVarType {
-    CA_VAR_TYPE_INT = 0,
-    CA_VAR_TYPE_REAL,
-    CA_VAR_TYPE_FUNC
-} CaVarTYPE
+typedef enum CaGuess {
+    CA_GUESS_INT = 0,
+    CA_GUESS_REAL,
+    CA_GUESS_OPER,
+    CA_GUESS_WORD
+} CaVarGuess;
+
+typedef union CaValue {
+    CaInt i,
+    CaChar c,
+    CaReal f,
+    CaObjPtr *p
+} CaValue;
 
 typedef struct CaVar {
-    void *data;
+    CaValue value;
     CaType type;
 } CaVar;
 
@@ -70,4 +88,16 @@ typedef union CaResult {
     CaReal r;
 } CaResult;
 
+
+#define ca_t_str(a)   ((a).type == CA_TYPE_STRING)
+#define ca_t_int(a)   ((a).type == CA_TYPE_INT)
+#define ca_t_real(a)  ((a).type == CA_TYPE_REAL)
+#define ca_t_dict(a)  ((a).type == CA_TYPE_DICT)
+#define ca_t_set(a)   ((a).type == CA_TYPE_SET)
+#define ca_t_list(a)  ((a).type == CA_TYPE_LIST)
+#define ca_t_tuple(a) ((a).type == CA_TYPE_TUPLE)
+
+// May change later
+#define ca_t_obj(a)   ((a).type == CA_TYPE_OBJ)
+#define ca_objtype(a) (((void *) (a).value)->type)
 #endif
